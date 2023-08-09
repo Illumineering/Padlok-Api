286c6c63d091819e137c8bfdef91bcd2e755b120
Thomas Durand <thomas.durand.verjat@gmail.com>
2023-08-09 19:42:53 +0200

    Using bot instead of no_reply

--- src/Handler/Feedback/MailFeedbackHandler.php
+++ src/Handler/Feedback/MailFeedbackHandler.php
@@ -21,7 +21,7 @@ final class MailFeedbackHandler implements FeedbackHandlerInterface
 
     public function handle(Feedback $feedback): void
     {
-        $from = new Address('no_reply@illumineering.fr', 'Illumineering Bot');
+        $from = new Address('bot@illumineering.fr', 'Illumineering Bot');
         if (Reason::Illumineering === $feedback->reason) {
             $to = 'hello@illumineering.fr'; // FIXME: maybe not hard-coding this?
         } else {
